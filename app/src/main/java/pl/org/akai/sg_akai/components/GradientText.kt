236c9fb00adb16c94edd7b446ec7c8aaca340ff4
SafeHeaded <m.patryk@outlook.com>
2022-04-23 16:13:21 +0200

    image and circle added

--- app/src/main/java/pl/org/akai/sg_akai/components/GradientText.kt
+++ app/src/main/java/pl/org/akai/sg_akai/components/GradientText.kt
@@ -14,7 +14,7 @@ import pl.org.akai.sg_akai.ui.theme.Green300
 import pl.org.akai.sg_akai.ui.theme.SmartGardenTypography
 
 @Composable
-fun GradientText(text: String) {
+fun GradientText(text: String, brush: Brush = Brush.horizontalGradient(listOf(Blue300, Green300))) {
     Text(
         text,
         style = SmartGardenTypography.h4,
@@ -22,7 +22,6 @@ fun GradientText(text: String) {
         modifier = Modifier
             .graphicsLayer(alpha = 0.99f)
             .drawWithCache {
-                val brush = Brush.horizontalGradient(listOf(Blue300, Green300))
                 onDrawWithContent {
                     drawContent()
                     drawRect(brush, blendMode = BlendMode.SrcAtop)
--- app/src/main/java/pl/org/akai/sg_akai/presentation/plant_statistics_screen/PlantStatisticsScreen.kt
+++ app/src/main/java/pl/org/akai/sg_akai/presentation/plant_statistics_screen/PlantStatisticsScreen.kt
@@ -1,16 +1,27 @@
 package pl.org.akai.sg_akai.presentation.plant_statistics_screen
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pl.org.akai.sg_akai.R
 import pl.org.akai.sg_akai.components.GradientText
+import pl.org.akai.sg_akai.ui.theme.Blue100
+import pl.org.akai.sg_akai.ui.theme.Green100
+import pl.org.akai.sg_akai.ui.theme.SmartGardenTypography
 import kotlin.jvm.internal.Intrinsics
 
 @Composable
@@ -21,19 +32,53 @@ fun PlantStatisticsScreen() {
         modifier = Modifier
             .fillMaxHeight()
             .fillMaxWidth()
+            .padding()
     ) {
         val image = painterResource(id = R.drawable.plant)
-        GradientText(text = "Nazwa rośliny")
+        GradientText(
+            text = "Nazwa rośliny",
+            brush = Brush.horizontalGradient(listOf(Blue100, Green100))
+        )
         Row {
-            Box(Modifier.fillMaxWidth(.5f)) {
+            Box(
+                Modifier
+                    .fillMaxWidth(.5f)
+            ) {
                 Image(
                     painter = image,
                     contentDescription = "A plant",
-                    contentScale = ContentScale.Fit
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxWidth(.6f)
+
                 )
             }
             Box(Modifier.fillMaxWidth(.5f)) {
-
+                Canvas(modifier = Modifier.size(400.dp), onDraw = {
+                    drawCircle(
+                        color = Green100,
+                        radius = 600f,
+                        center = Offset(size.width * 1.5f, size.height / 2f)
+                    )
+                })
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                        .padding(top = 50.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        "Sansewieria\ngwinejska",
+                        textAlign = TextAlign.Center,
+                        color = Color.White,
+                        style = SmartGardenTypography.button,
+                        fontWeight = FontWeight.Black,
+                        fontSize = 13.sp
+                    )
+                    Text(
+                        text = "20 tygodni",
+                        style = SmartGardenTypography.caption,
+                        fontWeight = FontWeight.Black
+                    )
+                }
             }
         }
     }
