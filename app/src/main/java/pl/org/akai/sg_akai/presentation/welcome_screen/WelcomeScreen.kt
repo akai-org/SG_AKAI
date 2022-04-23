fc32043d0544f8032c4f9001db5d8dad026074e2
SafeHeaded <m.patryk@outlook.com>
2022-04-23 12:35:18 +0200

    basic texts added

--- app/src/main/java/pl/org/akai/sg_akai/presentation/welcome_screen/WelcomeScreen.kt
+++ app/src/main/java/pl/org/akai/sg_akai/presentation/welcome_screen/WelcomeScreen.kt
@@ -1,9 +1,30 @@
 package pl.org.akai.sg_akai.presentation.welcome_screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import pl.org.akai.sg_akai.ui.theme.SmartGardenTypography
 
+@Preview
 @Composable
 fun WelcomeScreen() {
-    Text(text = "Welcome Screen")
+    Column {
+        Text("Smart Garden", style = SmartGardenTypography.h5, textAlign = TextAlign.Center)
+        Text(
+            modifier = Modifier.padding(horizontal = 20.dp),
+            text = "Wszystko czego potrzebujesz, by budować armię do ratowania Ziemi jest w Twojej kieszeni",
+            fontWeight = FontWeight.Thin,
+            style = SmartGardenTypography.h5,
+            textAlign = TextAlign.Center,
+        )
+    }
 }
